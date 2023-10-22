e5c5788e6f6791d13a9290a5b7f43ff40fc089c9
Suraj Suryabanshi <84841366+SurajSuryabanshi@users.noreply.github.com>
2023-10-22 18:16:08 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
      jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://Surajbwnxi:suraj123@cluster0.ra6mris.mongodb.net/"
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://Surajbwnxi:suraj123@Marketplace.nxqtcuo.mongodb.net/Marketplace?retryWrites=true&w=majority"
    
     };
     
